11371784b691d3ebe08e6fe79c1c8f2903f137e7
dtmacalalad <devinmacalaladdt@gmail.com>
2019-11-17 19:01:09 -0500

    Update Schema.sql

--- Schema.sql
+++ Schema.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE ProjPart3;
-USE ProjPart3;
+CREATE DATABASE IF NOT EXISTS `AirlineFlight`;
+USE `AirlineFlight`;
 
 CREATE TABLE Flight
 	(Flight INTEGER,
@@ -10,35 +10,16 @@ CREATE TABLE Flight
 	3letterID CHAR(3)
 	);
 
-INSERT INTO Flight (Flight, Departure_time, Days_of_operation, Arrival_time, 2letterID, 3letterID)
-VALUES (001, 140, "MTFS", 940, "AA", "JFK"); /* T=Tuesday,TH=Thursday, SU=Sunday ...
-										   AA=American Airlines
-					                       JFK=JFK Airport */
-INSERT INTO Flight (Flight, Departure_time, Days_of_operation, Arrival_time, 2letterID, 3letterID)
-VALUES (011,120, "MWTH",340,"DE","JFK"); /* DE=Delta */
-
-
 
 
 CREATE TABLE Airport
 	(3letterID CHAR(3)
 	);
 
-INSERT INTO Airport (3letterID)
-VALUES ("JFK");
-
-
-
-
 
 CREATE TABLE Airline
 	(2letterID CHAR(2)
 	);
-
-INSERT INTO Airline (2letterID)
-VALUES ("AA");
-INSERT INTO Airline (2letterID)
-VALUES ("DE");
 
 
 
@@ -48,28 +29,12 @@ CREATE TABLE Aircraft
 	2letterID CHAR(2)
 	);
 
-INSERT INTO Aircraft (Identifier, Maximum_capacity, 2letterID)
-VALUES (12, 232, "AA");
-INSERT INTO Aircraft (Identifier, Maximum_capacity, 2letterID)
-VALUES (14, 412, "DE");
-
-
-
-
 
 CREATE TABLE Seat
 	(Seat INTEGER,
 	Meal_ordered CHAR(20),
 	Flight INTEGER
 	);
-
-INSERT INTO Seat (Seat, Meal_ordered, Flight)
-VALUES (1, "Salad", 001);
-INSERT INTO Seat (Seat, Meal_ordered, Flight)
-VALUES (19, "Fish", 011);
-
-
-
 
 
 CREATE TABLE Ticket
@@ -79,49 +44,24 @@ CREATE TABLE Ticket
 	Date_of_purchase DATE
 	);
 
-INSERT INTO Ticket (Unique_number, Fare, Booking_fee, Date_of_Purchase)
-VALUES (1, 350.00, 15.00, 07-28-2019);
-INSERT INTO Ticket (Unique_number, Fare, Booking_fee, Date_of_Purchase)
-VALUES (23, 750.00, 23.50, 12-13-2018);
-
 
 CREATE TABLE Reservation
 	(Unique_number INTEGER,
 	Name CHAR(30)
 	);
 
-INSERT INTO Reservation (Unique_number, Name)
-VALUES (1, "Bob Ross");
 
-INSERT INTO Reservation (Unique_number, Name)
-VALUES (2, "Joe Davis");
-
-
-
-
-
-
-CREATE TABLE Account
-	(Name CHAR(30)
+CREATE TABLE account
+	(accountname CHAR(30),
+	 password CHAR(30)
+	 
 	);
-
-INSERT INTO Account (Name)
-VALUES ("Bob Ross");
-INSERT INTO Account (Name)
-VALUES ("Joe Davis");
-
-
-
-
-
 
 
 CREATE TABLE Domestic
 	(Flight INTEGER
 	);
 
-INSERT INTO Domestic (Flight)
-VALUES (001);
 
 
 
@@ -129,8 +69,6 @@ CREATE TABLE International
 	(Flight INTEGER
 	);
 
-INSERT INTO Domestic (Flight)
-VALUES (011);
 
 
 
@@ -138,8 +76,6 @@ CREATE TABLE RoundTrip
 	(Unique_number INTEGER
 	);
 
-INSERT INTO RoundTrip (Unique_number)
-VALUES (23);
 
 
 
@@ -147,7 +83,4 @@ CREATE TABLE Oneway
 	(Unique_number INTEGER
 	);
 
-INSERT INTO Oneway (Unique_number)
-VALUES (1);
 
-SELECT * FROM Flight;
