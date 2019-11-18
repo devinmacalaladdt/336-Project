a95cac5eb725e2cc2b4eaaa4ce0ae5e417d25d4b
Devin Macalalad <devinmacalaladdt@gmail.com>
2019-11-18 17:24:21 -0500

    funcitonal SQL

--- Schema.sql
+++ Schema.sql
@@ -12,17 +12,6 @@ CREATE TABLE IF NOT EXISTS account (
 );
 
 
-CREATE TABLE IF NOT EXISTS Flight
-	(Flight INTEGER,
-	Departure_time TIME,
-	Days_of_operation CHAR(20),
-	Arrival_time TIME,
-	2letterID CHAR(2),
-	3letterID CHAR(3),
-	PRIMARY KEY (Flight),
-	FOREIGN KEY (2letterID) REFERENCES Airline
-	);
-
 
 CREATE TABLE IF NOT EXISTS Airline
 	(2letterID CHAR(2),
@@ -33,6 +22,18 @@ CREATE TABLE IF NOT EXISTS Airport
 	(3letterID CHAR(3),
     PRIMARY KEY (3letterID)
 	);
+    
+    CREATE TABLE IF NOT EXISTS Flight
+	(Flight INTEGER,
+	Departure_time TIME,
+	Days_of_operation CHAR(20),
+	Arrival_time TIME,
+	2letterID CHAR(2),
+	3letterID CHAR(3),
+	PRIMARY KEY (Flight),
+	FOREIGN KEY (2letterID) REFERENCES Airline(2letterID)
+	);
+
 
 
 
@@ -41,7 +42,7 @@ CREATE TABLE IF NOT EXISTS Aircraft
 	Maximum_capacity INTEGER,
 	2letterID CHAR(2),
 	PRIMARY KEY (Identifier),
-	FOREIGN KEY (2letterID) REFERENCES Airline
+	FOREIGN KEY (2letterID) REFERENCES Airline(2letterID)
 	);
 
 
@@ -52,7 +53,7 @@ CREATE TABLE IF NOT EXISTS Seat
 	Meal_ordered CHAR(20),
 	Flight INTEGER,
 	PRIMARY KEY (Seat),
-	FOREIGN KEY (Flight) REFERENCES Flight
+	FOREIGN KEY (Flight) REFERENCES Flight(Flight)
 	);
 
 
@@ -69,8 +70,7 @@ CREATE TABLE IF NOT EXISTS Ticket
 CREATE TABLE IF NOT EXISTS Reservation
 	(Unique_number INTEGER,
 	Name CHAR(30),
-	PRIMARY KEY (Unique_number),
-	PRIMARY KEY (Name)
+	PRIMARY KEY (Unique_number,Name)
 	);
 
 
@@ -78,7 +78,7 @@ CREATE TABLE IF NOT EXISTS Reservation
 
 CREATE TABLE IF NOT EXISTS Domestic
 	(Flight INTEGER,
-	FOREIGN KEY (Flight) REFERENCES Flight
+	FOREIGN KEY (Flight) REFERENCES Flight(Flight)
 	);
 
 
@@ -86,7 +86,7 @@ CREATE TABLE IF NOT EXISTS Domestic
 
 CREATE TABLE IF NOT EXISTS International
 	(Flight INTEGER,
-	FOREIGN KEY (Flight) REFERENCES Flight
+	FOREIGN KEY (Flight) REFERENCES Flight(Flight)
 	);
 
 
@@ -94,7 +94,7 @@ CREATE TABLE IF NOT EXISTS International
 
 CREATE TABLE IF NOT EXISTS RoundTrip
 	(Unique_number INTEGER,
-	FOREIGN KEY (Unique_number) REFERENCES Ticket
+	FOREIGN KEY (Unique_number) REFERENCES Ticket(Unique_number)
 	);
 
 
@@ -102,7 +102,7 @@ CREATE TABLE IF NOT EXISTS RoundTrip
 
 CREATE TABLE IF NOT EXISTS Oneway
 	(Unique_number INTEGER,
-	FOREIGN KEY (Unique_number) REFERENCES Ticket
+	FOREIGN KEY (Unique_number) REFERENCES Ticket(Unique_number)
 	);
 
 
@@ -110,60 +110,61 @@ CREATE TABLE IF NOT EXISTS Operates
 	(2letterID CHAR(2),
 	3letterID CHAR(3),
     number_of INTEGER,
-    FOREIGN KEY (2letterID) REFERENCES Airline,
-    FOREIGN KEY (3letterID) REFERENCES Airport
+    FOREIGN KEY (2letterID) REFERENCES Airline(2letterID),
+    FOREIGN KEY (3letterID) REFERENCES Airport(3letterID)
 	);
     
 CREATE TABLE IF NOT EXISTS Owns
 	(2letterID CHAR(2),
 	Identifier INTEGER,
     quantity INTEGER,
-FOREIGN KEY (2letterID) REFERENCES Airline,
-FOREIGN KEY (3letterID) REFERENCES Airport
+FOREIGN KEY (2letterID) REFERENCES Airline(2letterID),
+FOREIGN KEY (Identifier) REFERENCES Aircraft(Identifier)
 	);
     
 CREATE TABLE IF NOT EXISTS Operates2
-	(2letterID CHAR(2),
+	(3letterID CHAR(3),
 	Identifier INTEGER,
     Flight INTEGER,
-FOREIGN KEY (Identifier) REFERENCES Aircraft,
-FOREIGN KEY (3letterID) REFERENCES Airport,
-FOREIGN KEY (Flight) REFERENCES Flight
+FOREIGN KEY (Identifier) REFERENCES Aircraft(Identifier),
+FOREIGN KEY (3letterID) REFERENCES Airport(3letterID),
+FOREIGN KEY (Flight) REFERENCES Flight(Flight)
 	);
 
 CREATE TABLE IF NOT EXISTS Destination
 	(3letterID CHAR(2),
     Flight INTEGER,
-	FOREIGN KEY (3letterID) REFERENCES Airport,
-	FOREIGN KEY (Flight) REFERENCES Flight
+	FOREIGN KEY (3letterID) REFERENCES Airport(3letterID),
+	FOREIGN KEY (Flight) REFERENCES Flight(Flight)
 	);
 
 CREATE TABLE IF NOT EXISTS Departure
 	(3letterID CHAR(2),
     Flight INTEGER,
-FOREIGN KEY (3letterID) REFERENCES Airport,
-	FOREIGN KEY (Flight) REFERENCES Flight
+FOREIGN KEY (3letterID) REFERENCES Airport(3letterID),
+	FOREIGN KEY (Flight) REFERENCES Flight(Flight)
 	);
 
 CREATE TABLE IF NOT EXISTS Associated
 	(Unique_number INTEGER,
     Flight INTEGER,
     Seat INTEGER,
-	FOREIGN KEY (Unique_number) REFERENCES Ticket,
-	FOREIGN KEY (Flight) REFERENCES Flight,
-	FOREIGN KEY (Seat) REFERENCES Seat
+	FOREIGN KEY (Unique_number) REFERENCES Ticket(Unique_number),
+	FOREIGN KEY (Flight) REFERENCES Flight(Flight),
+	FOREIGN KEY (Seat) REFERENCES Seat(Seat)
 	);
 
 CREATE TABLE IF NOT EXISTS Has
 	(Unique_number INTEGER,
-	FOREIGN KEY (Unique_number) REFERENCES Ticket
+	FOREIGN KEY (Unique_number) REFERENCES Ticket(Unique_number)
 	);
     
     
 CREATE TABLE IF NOT EXISTS ReservationPortfolio
 	(Name CHAR(30),
-	FOREIGN KEY (Name) REFERENCES account
+	FOREIGN KEY (Name) REFERENCES account(accountname)
 	);
     
+
 
 
